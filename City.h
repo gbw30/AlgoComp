7d6dace3ee95f1bfe4ad458d96fcc382ebc5d17e
Alicia Ellis <fr0gpuk3@gmail.com>
2024-12-03 06:48:19 -0500

    Removed an attribute in City.h

--- City.h
+++ City.h
@@ -1,21 +1,13 @@
-//
-// Created by Owner on 12/3/2024.
-//
-
-#ifndef ALGOCOMP_CITY_H
-#define ALGOCOMP_CITY_H
+#pragma once
 
 #include "string"
-
 using namespace std;
 
 struct City {
-
     // Attributes
     int cityID;
-    string name;
-    string state;
-    string country;
+    string cityName;
+    string stateName;
     float latitude;
     float longitude;
     int elevation;
@@ -25,6 +17,3 @@ struct City {
     // Constructor
     City(int id, string n, string st, string cn, float lat, float lon, int elev, int pop, string tz);
 };
-
-
-#endif //ALGOCOMP_CITY_H
