8ee6ff2e723aa5e3f2391b011380119fe1a7cf78
Yanka D <yankadeshkovski@Yankas-MacBook-Air.local>
2024-12-03 07:21:02 -0500

    Finished method for reading CSV

--- readerfile.cpp
+++ readerfile.cpp
@@ -19,6 +19,27 @@ vector<City> readFromCSV(string fileName)
     if (!file.is_open())
         cout << "File not opened!" << endl;
 
+    string line;
+    getLine(file, line);
+
+    while (getLine(file, line))
+    {
+        stringstream ss(line);
+        City place;
+        string temp;
+
+        getline(ss, temp, ','), place.cityID = stoi(temp);
+        getline(ss, place.cityName, ',');
+        getline(ss, place.stateName, ',');
+        getline(ss, temp, ','), place.population = stoi(temp);
+        getline(ss, temp, ','), place.elevation = stoi(temp);
+        getline(ss, temp, ','), place.longitude = stod(temp);
+        getline(ss, temp, ','); place.latitude = stod(temp);
+        getline(ss, temp, ','); place.timezone = stoi(temp);
+
+        cities.push_back(place);
+    }
+    file.close();
     return cities;
 }
 
