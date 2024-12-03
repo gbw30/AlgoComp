e1381b850d17b80438c551f9a4f96dfd9592273a
Yanka D <yankadeshkovski@Yankas-MacBook-Air.local>
2024-12-03 07:28:17 -0500

    readerfile.h addition and edit

---
+++ readerfile.h
@@ -0,0 +1,15 @@
+//
+// Created by Yanka Deshkovski on 12/3/24.
+//
+
+#ifndef ALGOCOMP_READERFILE_H
+#define ALGOCOMP_READERFILE_H
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+vector<City> readFromCSV(string fileName);
+
+#endif //ALGOCOMP_READERFILE_H
