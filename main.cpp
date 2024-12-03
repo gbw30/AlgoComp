819f195250601ff3175f6c68f889e5e0641dd9b4
Yanka D <yankadeshkovski@Yankas-MacBook-Air.local>
2024-12-03 08:36:23 -0500

    Edits to readFromCSV

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 // Created by Gabriel Wong Chong on 12/3/2024.
 //
 
-#include "iostream"
+#include <iostream>
 #include "City.h"
 #include "readerfile.h"
 
@@ -13,8 +13,7 @@ using namespace std;
 
 int main()
 {
-    vector<City> cities = readFromCSV("City1.txt");
-    // for (int i = 0; i < cities.size(); i++)
-        // cout << cities[i]->cityName << endl;
-
+    vector<City> cities = readFromCSV("../city1.txt");
+    for (int i = 0; i < cities.size() / 1000; i++)
+        cout << cities[i].getCityName() << endl;
 }
